bb20d3f6b332577ab686d34a18b42456cce0ef80
Chelsey Dolan <chelseymarie6@gmail.com>
2018-03-10 15:09:59 -0800

    removed default styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+
 
 import Main from './pages/Main';
 import NoMatch from "./pages/NoMatch";
