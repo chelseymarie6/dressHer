c526b5473262771bc42e170ca6156991b4bde0ff
lubna qadah <lubna.alqadah@gmail.com>
2018-03-10 15:16:52 -0800

    added api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const items = require("../../controllers/items");
 
+//to get all the items
 router.route("/all/")
   .get(items.findAll);
 
 
+//to create new item
+router.route("/new")
+  .post(items.create);
+
+
 module.exports = router;
+
